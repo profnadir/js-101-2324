3a01c5f8f4f5f0d70321cfbf1d8375debc36765c
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-12-19 13:46:00 +0100

    s4

---
+++ s5/scripts.js
@@ -0,0 +1,56 @@
+/* function somme( a , b){
+    return a+b;
+} */
+
+
+
+/* function carre(x){
+    a = "hi dev101"
+    
+    return x*x;
+} */
+
+
+
+/* alert(carre(2));
+
+console.log(a) */
+
+
+
+
+
+
+/* function myFunction(nom) {
+    console.log("hi "+nom)
+} */
+
+//myFunction("nadir")
+
+/* document.write(somme(4,7))
+alert(somme(4,7))
+console.log(somme(4,7))
+ */
+
+function exo() {
+    let rayon = parseFloat(document.getElementById('rayon').value);
+    document.getElementById("resultat").innerHTML = surface(rayon);
+}
+
+//(1)
+function surface(rayon) {
+    return 3.14 * rayon * rayon;
+}
+
+/*  (1)=== rayon => 3.14 * rayon * rayon; */
+
+
+function nomFonction(p1, p2){
+    // ici code
+}
+
+let a = 5;
+
+let surface = (p1, p2) => { 
+    //ici code
+}
